2c9249f47ceee963fb44f2aa0765214fa8910df1
SanguineTitan57 <69134964+SanguineTitan57@users.noreply.github.com>
2026-01-09 14:23:04 +0300

    refactor: Update Providers.tsx
    - Added Provider Props
    - Added a null/undefined check to ensure enviraonment variable is not
      undefined

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -1,19 +1,33 @@
 import '@rainbow-me/rainbowkit/styles.css'
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { WagmiProvider } from 'wagmi'
-import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains'
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  sepolia,
+} from 'wagmi/chains'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+interface ProviderProps {
+  children: React.ReactNode
+}
+
+const projectID = process.env.WALLET_CONNECT_PROJECT_ID
+if (!projectID) throw new Error('Project ID is not defined!')
 
 const config = getDefaultConfig({
   appName: 'My RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [mainnet, polygon, optimism, arbitrum, base],
+  projectId: projectID, // ensures that the environment variable is always available
+  chains: [mainnet, sepolia, polygon, optimism, arbitrum, base],
   ssr: true, // If your dApp uses server side rendering (SSR)
 })
 
 const queryClient = new QueryClient()
 
-const Providers = ({children}: React.ReactNode) => {
+const Providers = ({ children }: ProviderProps) => {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
@@ -23,4 +37,4 @@ const Providers = ({children}: React.ReactNode) => {
   )
 }
 
-export default Providers;
+export default Providers
